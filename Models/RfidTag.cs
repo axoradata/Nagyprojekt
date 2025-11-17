ef4d4a64736d34c538a050ebc721d6c7fb27e705
axoradata <axoradata@gmail.com>
2025-11-17 11:16:23 +0100

    Add files via upload

---
+++ Models/RfidTag.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NagyprojektBackend.Models
+{
+    public class RfidTag
+    {
+        [Key]
+        public int Id { get; set; }
+        public string TagId { get; set; } = null!;
+        public string? Owner { get; set; }
+        public bool Active { get; set; } = true;
+    }
+}
