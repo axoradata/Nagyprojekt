dd932be2cd6388ada9b21f18ffbac5717fdce815
GoldenFox013 <borok.maja@gmail.com>
2026-01-27 18:46:22 +0100

    Delete Mobile directory

--- Mobile/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mobilapp-ionic',
-  webDir: 'dist'
-};
-
-export default config;
--- Mobile/src/composables/useFocus.ts
+++
@@ -1,8 +0,0 @@
-import { onMounted } from 'vue'
-
-export function useFocus() {
-  onMounted(() => {
-    const firstInput = document.querySelector('ion-input')
-    if(firstInput) (firstInput as any).setFocus()
-  })
-}
